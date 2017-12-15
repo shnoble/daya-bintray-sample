eef9c60ac86bc928412dc439c971e94b2c010269
홍석현/클라우드개발팀/NE <shhong@nhnent.com>
2017-12-15 20:01:23 +0900

    Create project

---
+++ daya-bintray-core/src/main/java/com/daya/bintray/core/SdkVersion.java
@@ -0,0 +1,11 @@
+package com.daya.bintray.core;
+
+/**
+ * Created by shhong on 2017. 12. 15..
+ */
+
+public class SdkVersion {
+    public static String getVersion() {
+        return BuildConfig.VERSION_NAME;
+    }
+}
---
+++ daya-bintray-sdk/src/main/java/com/daya/bintray/sdk/SdkVersion.java
@@ -0,0 +1,13 @@
+package com.daya.bintray.sdk;
+
+import android.os.Build;
+
+/**
+ * Created by shhong on 2017. 12. 15..
+ */
+
+public class SdkVersion {
+    public static String getVersion() {
+        return BuildConfig.VERSION_NAME;
+    }
+}
